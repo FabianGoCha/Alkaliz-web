17e5a51ac60fba71aac88ed29a9b61f08a5e42b2
Fabián Eduardo González Chacón <fagocha1999@gmail.com>
2024-07-01 17:04:31 -0600

    burger button
    intento de responsive nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ function App()  {
   return(
     
     <Router>
-    <Header/>
-    <CarouselFadeExample/>
-    <Footer/>
+      <Header/>
+      <CarouselFadeExample/>
+      <Footer/>
     </Router>
   );
 }
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,20 +1,46 @@
+import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { GiHamburgerMenu } from "react-icons/gi";
 import './Header.css'
 
-function Header() {
+const Header = () => {
+
+    const [showNavbar, setShowNavbar] = useState(false)
+
+    const handleShowNavbar = () => {
+        setShowNavbar(!showNavbar)
+    }
 
     return(
         <header className='header'> 
             <h1>Alkaliz</h1>
-            <div className="Logo"><a href="#">Logo</a></div>
-            <nav>
-                <ul>
-                    <li><a href="#">Inicio</a></li>
-                    <li><a href="#">Acerca de</a></li>
-                    <li><a href="#">Contacto</a></li>
-                    <li><a href="#">Trabajos</a></li>
-                </ul>
+            <nav className='navbar'>
+                <div className='container'>
+                    <div className="Logo">
+                        <a to="/">Logo</a>
+                    </div>
+                    <div className='menu-icon' onClick={handleShowNavbar}>
+                        <GiHamburgerMenu size={50}/>
+                    </div>
+                    <div className={`nav-elements  ${showNavbar && 'active'}`}>    
+                        <ul>
+                            <li>
+                                <NavLink to="/">Inicio</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/acerca">Acerca de</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/contacto">Contacto</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/trabajos">Trabajos</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </nav>
-            <hr></hr>
+            
         </header>
     );
 }
